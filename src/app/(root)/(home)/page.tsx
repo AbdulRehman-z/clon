7d53a569acf9f07083182ebaf5cba582cb59cd06
AbdulRehman-z <yousafbhaikhan10@gmail.com>
2024-04-05 16:42:24 +0500

    Add new pages to the home directory

--- src/app/(root)/(home)/page.tsx
+++ src/app/(root)/(home)/page.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <section className="flex flex-col size-full text-white">
+      <h1 className="font-bold text-3xl">Home</h1>
+    </section>
+  );
 };
 
 export default Home;
