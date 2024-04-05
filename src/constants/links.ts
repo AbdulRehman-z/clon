8abdf37957e5176f87698bad68035d00116670f9
AbdulRehman-z <yousafbhaikhan10@gmail.com>
2024-04-05 16:41:42 +0500

    Add sidebar links in links.ts

---
+++ src/constants/links.ts
@@ -0,0 +1,27 @@
+export const sideBarLinks = [
+  {
+    lable: "Home",
+    route: "/",
+    icon: "/icons/Home.svg",
+  },
+  {
+    lable: "Upcoming",
+    route: "/upcoming",
+    icon: "/icons/upcoming.svg",
+  },
+  {
+    lable: "Previous",
+    route: "/previous",
+    icon: "/icons/previous.svg",
+  },
+  {
+    lable: "Recordings",
+    route: "/recordings",
+    icon: "/icons/recordings.svg",
+  },
+  {
+    lable: "Personal Room",
+    route: "/personal-room",
+    icon: "/icons/add-personal.svg",
+  },
+];
