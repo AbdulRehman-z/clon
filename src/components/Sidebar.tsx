ebff059d8867f86c669bd3ec7bbe730347362c1c
AbdulRehman-z <yousafbhaikhan10@gmail.com>
2024-04-05 16:42:31 +0500

    Update Sidebar component to include sidebar links and use client-side navigation

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,7 +1,40 @@
-import React from "react";
+"use client";
+
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { sideBarLinks } from "../constants/links";
 
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  const pathname = usePathname();
+
+  return (
+    <section className="sticky left-0 top-0 flex flex-col justify-between h-screen w-fit p-6 pt-24 bg-dark-1 max-sm:hidden max-lg:w-[264px]">
+      <div className="flex flex-1 flex-col gap-6">
+        {sideBarLinks.map((item) => {
+          const isActive =
+            pathname === item.route || pathname.startsWith(`${item.route}/`);
+
+          return (
+            <Link
+              key={item.route}
+              href={item.route}
+              className={cn(
+                "flex items-center gap-4 p-4 rounded-lg justify-start",
+                isActive ? "bg-blue-1" : "hover:bg-dark-2"
+              )}
+            >
+              <Image src={item.icon} alt={item.lable} width={24} height={24} />
+              <span className="text-white text-lg max-sm:hidden">
+                {item.lable}
+              </span>
+            </Link>
+          );
+        })}
+      </div>
+    </section>
+  );
 };
 
 export default Sidebar;
