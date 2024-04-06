0bf209efc4247fffc7f1145fef8bb5ddfe8a7a5f
AbdulRehman-z <yousafbhaikhan10@gmail.com>
2024-04-06 21:32:35 +0500

    Update page styles in personal-room and previous components

--- src/app/(root)/(home)/personal-room/page.tsx
+++ src/app/(root)/(home)/personal-room/page.tsx
@@ -1,6 +1,6 @@
 const Personal = () => {
   return (
-    <section className="flex flex-col size-full text-white">
+    <section className="flex flex-col size-full text-white pt-10">
       <h1 className="font-bold text-3xl">Personal</h1>
     </section>
   );
--- src/app/(root)/(home)/previous/page.tsx
+++ src/app/(root)/(home)/previous/page.tsx
@@ -1,6 +1,6 @@
 const Previous = () => {
   return (
-    <section className="flex flex-col size-full text-white">
+    <section className="flex flex-col size-full text-white pt-10">
       <h1 className="font-bold text-3xl">Previous</h1>
     </section>
   );
