24ae4910d32160a02dfbe8d8738f5fe2b7925408
AbdulRehman-z <yousafbhaikhan10@gmail.com>
2024-04-06 21:33:19 +0500

    Refactor Navbar component to include responsive navigation

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,28 @@
+import Image from "next/image";
+import Link from "next/link";
+import MobileNavbar from "./MobileNavbar";
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className="flex-between fixed w-full px-6 py-4 lg:px-10 z-50 bg-dark-1">
+      <Link href={"/"} className="flex items-center gap-1">
+        <Image
+          src="/icons/logo.svg"
+          alt="logo"
+          width={32}
+          height={32}
+          className=" max-sm:size-10"
+        />
+        <p className="text-white font-extrabold text-[26px] max-sm:hidden">
+          Clon
+        </p>
+      </Link>
+
+      <div>
+        <MobileNavbar />
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
